3bc8816d85b148bb5a3ce57ff9ee7256d4057893
Bolla <Srinivas_Bolla@Dellteam.com>
2019-02-13 23:06:21 +0530

    initial commit

---
+++ src/main/java/com/techprimers/kafka/springbootkafkaconsumerexample/listener/KafkaConsumer.java
@@ -0,0 +1,15 @@
+package com.techprimers.kafka.springbootkafkaconsumerexample.listener;
+
+import com.techprimers.kafka.springbootkafkaconsumerexample.model.User;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "Kafka_Example", group = "group_id")
+    public void consume(String message) {
+        System.out.println("Consumed message: " + message);
+    }
+
+}
